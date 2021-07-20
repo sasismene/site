e997471cca93f044ef8be152f95e7ec012d0ef77
sasismene <50590996+sasismene@users.noreply.github.com>
2021-07-20 12:15:36 +0200

    Add files via upload

---
+++ a/redcraft/red.js
@@ -0,0 +1,53 @@
+const url = "https://api.minetools.eu/ping/play.redcraft.it";
+const players = $("#onlinePlayers");
+const playerList = $("#playerList");
+
+$(window).on("load", pingServer);
+
+function pingServer() {
+    $.getJSON(url, function (response) {
+        setTimeout(pingServer, 1e3);
+
+        // ERROR
+        if (response.error) {
+            // PLAYER COUNT
+            players.text("Server offline ");
+            document.getElementById("status").style.color = "red";
+
+            // EMPTY TABLE
+            playerList.html('');
+            $('<tr>').append(
+                $('<td>').html(`<img src='https://minotar.net/helm/HeroBrine/20'>`),
+                $('<td>').text('NONE'),
+                $('<td>').text('NONE'),
+            ).appendTo(playerList);
+
+            return
+        }
+
+        // PLAYER COUNT
+        players.text("Giocatori Online: " + response.players.online + " / " + response.players.max);
+        document.getElementById("status").style.color = "green";
+
+        if (response.players.online == 0) {
+            // EMPTY SERVER
+            playerList.html('');
+            $('<tr>').append(
+                $('<td>').html(`<img src='https://minotar.net/helm/HeroBrine/20'>`),
+                $('<td>').text('NONE'),
+                $('<td>').text('NONE'),
+            ).appendTo(playerList);
+            return
+        }
+
+        // CREATE TABLE
+        playerList.html('');
+        response.players.sample.forEach((p, i) => {
+            $('<tr>').append(
+                $('<td>').html(`<img src='https://crafatar.com/avatars/${p.id}?size=20&default=MHF_Steve'>`),
+                $('<td>').text(p.name),
+                $('<td>').text(p.id)
+            ).appendTo(playerList);
+        });
+    })
+}
---
+++ a/search/red.js
@@ -0,0 +1,52 @@
+const url = "https://api.minetools.eu/ping/play.redcraft.it";
+const players = $("#onlinePlayers");
+const playerList = $("#player");
+
+$(window).on("load", pingServer);
+
+function pingServer () {
+    $.getJSON(url, function (response) {
+        setTimeout(pingServer, 1e3);
+
+        // ERROR
+        if (response.error) {
+            // PLAYER COUNT
+            players.text("Server offline");
+
+            // EMPTY TABLE
+            playerList.html('');
+            $('<tr>').append(
+                $('<td>').html(`<img src='https://minotar.net/helm/HeroBrine/20'>`),
+                $('<td>').text('NONE'),
+                $('<td>').text('NONE')
+            ).appendTo(playerList);
+
+            return
+        }
+        
+        
+
+        // PLAYER COUNT
+        players.text("Giocatori Online: " + response.players.online + " / " + response.players.max);
+        if (response.players.online==0) {
+            // EMPTY SERVER
+            playerList.html('');
+            $('<tr>').append(
+                $('<td>').html(`<img src='https://minotar.net/helm/HeroBrine/20'>`),
+                $('<td>').text('NONE'),
+                $('<td>').text('NONE')
+            ).appendTo(playerList);
+            return
+        }
+        
+        // CREATE TABLE
+        playerList.html('');
+        response.players.sample.forEach((p,i) => {
+            $('<tr>').append(
+                $('<td>').html(`<img src='https://crafatar.com/avatars/${p.id}?size=20&default=MHF_Steve'>`),
+                $('<td>').text(p.name),
+                $('<td>').text(p.id)
+            ).appendTo(playerList);
+        });
+    })
+}
